f77c9a211a678988544f3de6ba9865f240155a25
Muhammad Jazlan Khan <142224933+MJazlanKhan@users.noreply.github.com>
2024-07-04 17:10:35 +0500

    Update AllPostsSingleCompany.js

--- client/src/Pages/Company/AllPostsSingleCompany.js
+++ client/src/Pages/Company/AllPostsSingleCompany.js
@@ -12,7 +12,7 @@ const AllPostsSingleCompany = () => {
   useEffect(() => {
     const fetchBlogPosts = async () => {
       try {
-        const response = await axios.get(`http://localhost:9000/api/v1/company/posts/${userId}`);
+        const response = await axios.get(`https://job-portal-app-gug2.onrender.com/api/v1/company/posts/${userId}`);
         if (response.status === 200) {
           const data = response.data;
           setJobPosts(data);
