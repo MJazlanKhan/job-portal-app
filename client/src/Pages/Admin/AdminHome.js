6fb68de5d0c85193fd5aaca44ba9551e4f340bba
Muhammad Jazlan Khan <142224933+MJazlanKhan@users.noreply.github.com>
2024-07-04 17:06:21 +0500

    Update AdminHome.js

--- client/src/Pages/Admin/AdminHome.js
+++ client/src/Pages/Admin/AdminHome.js
@@ -22,7 +22,7 @@ const AdminHome = () => {
   useEffect(() => {
     const fetchBlogPosts = async () => {
       try {
-        const response = await axios.get('http://localhost:9000/api/v1/allUsers');
+        const response = await axios.get('https://job-portal-app-gug2.onrender.com/api/v1/allUsers');
         if (response.status === 200) {
           const data = response.data;
           setusers(data);
